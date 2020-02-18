b436bd0136645b3da0c99d1cd88f6d6181b7a441
Juan Manuel Martin Jimenez <juanmamap@gmail.com>
2020-02-18 20:43:51 +0100

    Subir Pr-1 para terminar en casa

---
+++ pr_1_git.R
@@ -0,0 +1,36 @@
+##### Ejercicio 1.1.3 citar R
+
+citation()
+
+##### Ejercicio 1.2.1 Variable para cada tipo de objeto
+
+varNum <- 6
+varTex <- "Hola"
+varFac <- factor("hola","adios")
+varBol <- a < b
+
+##### Ejercicio 1.2.2 Operaciones aritmeticas
+
+6 + 5
+a <- 6
+b <-  5 
+suma <- a + b
+suma
+c <- "Hola"
+d <- "Adios"
+sumaTex <- 
+  sumaTex
+
+##### Ejercicio 1.2.3 Generar vectores y resumir estadisticas
+
+vecA <- c(6311, 6564, 6598, 6629, 6699, 621, 65,677)
+vecB <- c(1:9)
+vecC <- rep(c(1:5), each = 2)
+vecD <- rep(c(1:4), times = 3)
+vecE <- rep(c(2,3,1,5,6,7), c(3,3,3,7,1,3))
+
+##### Ejercicio 1.2.4 Matrices
+
+matriz1 <- rnorm(matrix(nrow = 20,ncol = 10))
+
+seleccion1 <- matriz1[3,5]
